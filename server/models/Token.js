c619fbf2ef42f0e37e1de4a66029a829340d628c
suprefuner <suprefuner@gmail.com>
2023-01-31 21:30:15 +0800

    first commit

---
+++ server/models/Token.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose")
+
+const tokenSchema = new mongoose.Schema(
+  {
+    refreshToken: {
+      type: String,
+      required: true,
+    },
+    ip: {
+      type: String,
+      required: true,
+    },
+    userAgent: {
+      type: String,
+      required: true,
+    },
+    isValid: {
+      type: Boolean,
+      default: true,
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const Token = mongoose.model("Token", tokenSchema)
+module.exports = Token
---
+++ server/utils/nodemailerConfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+  host: "smtp.ethereal.email",
+  port: 587,
+  auth: {
+    user: "euna.ferry@ethereal.email",
+    pass: "uQg5Q77Mf4eWu2zYCQ",
+  },
+}
